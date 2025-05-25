2cf00cdca68b15812eff9c7276e2d344c40a880d
Vishav <ge54nex@mytum.de>
2025-05-25 17:13:43 +0200

    feat: add cdk drag and drop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { FormEditorComponent } from './components/form-editor/form-editor.component';
 
 @Component({
   selector: 'forms-ai-app-root',
-  imports: [ItemListComponent],
+  imports: [ItemListComponent, FormEditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/components/item-list/item-list.component.ts
+++ client/src/app/components/item-list/item-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input, InputSignal } from '@angular/core';
+import {CdkDragDrop, DragDropModule} from '@angular/cdk/drag-drop';
 import { QuestionTypeCards } from '../../configs/question-type-cards.config';
 import { ItemCardComponent } from './item-card/item-card.component';
 
 @Component({
   selector: 'forms-ai-item-list',
-  imports: [ItemCardComponent],
+  imports: [ItemCardComponent, DragDropModule],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.css',
 })
