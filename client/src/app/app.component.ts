3721de45e9ef00438746e59d2826773795e73e87
Vishav <ge54nex@mytum.de>
2025-05-25 10:24:39 +0200

    chore: formatting

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ItemListComponent } from "./components/item-list/item-list.component";
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @Component({
   selector: 'forms-ai-app-root',
