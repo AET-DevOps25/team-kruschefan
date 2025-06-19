544dd6f0d4d61655b219f16216f22f84b7475d6e
Vishav <ge54nex@mytum.de>
2025-06-19 18:36:51 +0200

    feat: add home screen and user management

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'forms-ai-app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Forms-AI';
 }
