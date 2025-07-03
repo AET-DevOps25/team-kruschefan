d97bac04c4a574f67c5c457e529389e132d32440
Jay <jay0816@outlook.com>
2025-07-03 19:49:10 +0200

    relax lint for test files

--- web-client/eslint.config.js
+++ web-client/eslint.config.js
@@ -1,13 +1,13 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const prettier = require("eslint-config-prettier");
-const eslintPluginPrettier = require("eslint-plugin-prettier");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-config-prettier');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     plugins: {
       prettier: eslintPluginPrettier,
     },
@@ -20,31 +20,40 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "prettier/prettier": "error", // runs prettier as an ESLint rule
-      "@angular-eslint/directive-selector": [
-        "error",
+      'prettier/prettier': 'error', // runs prettier as an ESLint rule
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "forms-ai",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'forms-ai',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "forms-ai",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'forms-ai',
+          style: 'kebab-case',
         },
       ],
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
     rules: {},
+  },
+  {
+    files: ['**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@angular-eslint/component-selector': 'off',
+      '@angular-eslint/component-class-suffix': 'off',
+    },
   },
 );
