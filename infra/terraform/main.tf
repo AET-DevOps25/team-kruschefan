b86dd879e0f990b2084d816f204bba6d60108b21
Jay <jay0816@outlook.com>
2025-07-12 23:47:52 +0200

    update scripts

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,13 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Define an SSH key pair for connecting to the EC2 instance
 resource "aws_key_pair" "team_kruschefan_key" {
-  key_name   = var.ssh_key_name # Name visible in AWS Console
-  public_key = file(var.public_key_path) # Path to your local public key
+  key_name   = var.ssh_key_name
+  public_key = file(var.public_key_path)
 }
 
-# Create a security group allowing SSH and HTTP access
 resource "aws_security_group" "team_kruschefan_sg" {
   name        = "ec2-ssh-web"
   description = "Allow SSH and HTTP"
@@ -17,76 +15,40 @@ resource "aws_security_group" "team_kruschefan_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open SSH from anywhere
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open HTTP from anywhere
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" # Allow all outbound traffic
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Define the EC2 instance to launch
 resource "aws_instance" "team_kruschefan_ec2" {
-  ami           = var.ami_id # Amazon Linux 2 AMI in us-east-1
-  instance_type = var.instance_type # Free tier
-  key_name          = aws_key_pair.team_kruschefan_key.key_name # Use the created SSH key
-  security_groups   = [aws_security_group.team_kruschefan_sg.name] # Attach the security group
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.team_kruschefan_key.key_name
+
+  vpc_security_group_ids = [aws_security_group.team_kruschefan_sg.id]
 
   tags = {
-    Name = "team-kruschefan-ec2" # Tag in the AWS Console
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo Waiting for SSH...",
-      "while ! nc -z localhost 22; do sleep 5; done"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = var.ami_user
-      private_key = file(var.private_key_path)
-      host        = self.public_ip
-    }
+    Name = "team-kruschefan-ec2"
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_CONFIG=../ansible/ansible.cfg ansible-playbook -i ${self.public_ip}, -u ${var.ami_user} --private-key ${var.private_key_path} ../ansible/playbook.yml"
-
-    connection {
-      type        = "ssh"
-      user        = var.ami_user
-      private_key = file(var.private_key_path)
-      host        = self.public_ip
-    }
+    command = "echo 'EC2 instance ${self.public_ip} created.'"
   }
-}
 
-# KMS
-resource "aws_kms_key" "team_kruschefan_kms" {
-  description         = "KMS key for encrypting secrets"
-  deletion_window_in_days = 10
-  enable_key_rotation = true
-}
-
-# Secrets
-resource "aws_secretsmanager_secret" "team_kruschefan_credentials" {
-  name        = "team/credentials"
-  description = "Database username and password"
-  kms_key_id  = aws_kms_key.team_kruschefan_kms.arn
-}
-
-resource "aws_secretsmanager_secret_version" "team_kruschefan_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.team_kruschefan_credentials.id
-  secret_string = var.secret_string
+  lifecycle {
+    ignore_changes = [user_data]
+  }
 }
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,14 +1,9 @@
-output "instance_ip" {
-  description = "Public IP of the EC2 instance"
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
   value       = aws_instance.team_kruschefan_ec2.public_ip
 }
 
-output "kms_key_id" {
-  description = "KMS key ID"
-  value       = aws_kms_key.team_kruschefan_kms.arn
-}
-
-output "secret_arn" {
-  description = "ARN of the created secret"
-  value       = aws_secretsmanager_secret.team_kruschefan_credentials.arn
+output "ssh_connection" {
+  description = "SSH connection string"
+  value       = "ubuntu@${aws_instance.team_kruschefan_ec2.public_ip}"
 }
