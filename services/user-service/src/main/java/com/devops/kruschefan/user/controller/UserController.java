4746aed47ed3864a75354e2a38915f03fadb9cc2
Stefan Dimitrov <svdimitrv@gmail.com>
2025-07-14 19:43:17 +0200

    Secure user service

--- services/user-service/src/main/java/com/devops/kruschefan/user/controller/UserController.java
+++ services/user-service/src/main/java/com/devops/kruschefan/user/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.devops.kruschefan.user.controller;
 
 import com.devops.kruschefan.openapi.api.UserApi;
-import com.devops.kruschefan.openapi.api.UserApiDelegate;
+import com.devops.kruschefan.openapi.model.UserCreateRequest;
+import com.devops.kruschefan.openapi.model.UserResponse;
+import com.devops.kruschefan.openapi.model.UserUpdateRequest;
 import com.devops.kruschefan.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +20,27 @@ public class UserController implements UserApi {
     private final UserService userService;
 
     @Override
-    public UserApiDelegate getDelegate() {
-        return userService;
+    public ResponseEntity<UserResponse> createUser(UserCreateRequest userCreateRequest) {
+        return userService.createUser(userCreateRequest);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteUser(String username) {
+        return userService.deleteUser(username);
+    }
+
+    @Override
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @Override
+    public ResponseEntity<UserResponse> getUser(String username) {
+        return userService.getUser(username);
+    }
+
+    @Override
+    public ResponseEntity<UserResponse> updateUser(String username, UserUpdateRequest userUpdateRequest) {
+        return userService.updateUser(username, userUpdateRequest);
     }
 }
