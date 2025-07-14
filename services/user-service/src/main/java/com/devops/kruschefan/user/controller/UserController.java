a8273eccc133eec3e8d520b29b9d45f5f7404a3b
Stefan Dimitrov <svdimitrv@gmail.com>
2025-07-14 19:14:45 +0200

    Secure client service

--- services/user-service/src/main/java/com/devops/kruschefan/user/controller/UserController.java
+++ services/user-service/src/main/java/com/devops/kruschefan/user/controller/UserController.java
@@ -4,10 +4,12 @@ import com.devops.kruschefan.openapi.api.UserApi;
 import com.devops.kruschefan.openapi.api.UserApiDelegate;
 import com.devops.kruschefan.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('client_user')")
 public class UserController implements UserApi {
 
     private final UserService userService;
