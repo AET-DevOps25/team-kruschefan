b7816c4e3832d6c5fd6d343350b899448572f855
Vishav <ge54nex@mytum.de>
2025-07-14 18:07:11 +0200

    fix: fix url

--- web-client/src/environments/environment.k8s.ts
+++ web-client/src/environments/environment.k8s.ts
@@ -8,7 +8,7 @@ export const environment = {
   userApiUrl:
     'http://api-gateway.team-kruschefan-project.svc.cluster.local:8080',
   keycloak: {
-    issuer: 'https://team-kruschefan.local/keycloak',
+    issuer: 'https://keycloak.team-kruschefan.local',
     realm: 'forms-ai',
     clientId: 'angular-frontend',
   },
