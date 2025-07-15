2ba6dc0df3466da69b49f1edb173c2b2d3cd42a7
Stefan Dimitrov <svdimitrv@gmail.com>
2025-07-15 23:31:40 +0200

    Update keycloak url

--- web-client/src/environments/environment.k8s.ts
+++ web-client/src/environments/environment.k8s.ts
@@ -5,7 +5,7 @@ export const environment = {
   formApiUrl: 'http://team-kruschefan.local/api',
   userApiUrl: 'http://team-kruschefan.local/api',
   keycloak: {
-    issuer: 'http://keycloak.team-kruschefan.local',
+    issuer: 'http://keycloak.team-kruschefan-project.svc.cluster.local',
     realm: 'forms-ai',
     clientId: 'angular-frontend',
   },
